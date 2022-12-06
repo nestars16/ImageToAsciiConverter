a23fdb9f96f7ac7b98275fd3caa961276591cb48
nestars16 <nesmb16@gmail.com>
2022-12-06 17:53:22 -0600

    Included CMAKE image manipulation lib and began work on image parser

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <lib\include\Magick++.h>
 //cmake -S ..\..\ -B . -G "MinGW Makefiles"
 
-int main()
+int main( int /*argc*/, char ** argv)
 {
+
+    Magick::InitializeMagick(*argv);
+    
     std::cout << "lol\n";
     return 0;
 }
