8615da87ba236f479cd0724d3b68a16c920d3aed
nestars16 <nesmb16@gmail.com>
2022-12-08 12:54:10 -0600

    Changing Image Processing Library

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <ImageParser.h>
+
 //cmake -S ..\..\ -B . -G "MinGW Makefiles"
 
 int main( int /*argc*/, char ** argv)
 {
-
-    Magick::InitializeMagick(*argv);
-
-    ImageParser firstTest{static_cast<std::string_view>("img/thumbsUpEmoji")};
-
     
     return 0;
 }
